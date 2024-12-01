9969746af619a2813ecfe3d542496836537f69e4
Calus <109810259+Calus426@users.noreply.github.com>
2024-12-01 18:06:22 +0800

    Time: 548 ms (86.49%), Space: 0B (100%) - LeetHub

---
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -0,0 +1,10 @@
+
+
+SELECT ROUND(COUNT(DISTINCT player_id)/(SELECT COUNT(DISTINCT player_id )FROM ACTIVITY),2) AS fraction
+FROM ACTIVITY
+WHERE
+(player_id,DATE_SUB(event_date,INTERVAL 1 DAY))
+IN (SELECT player_id, MIN(event_date)
+FROM Activity
+GROUP BY player_id)
+
