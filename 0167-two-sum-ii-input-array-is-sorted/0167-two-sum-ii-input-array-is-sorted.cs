8a19f8220520d879c7d3b4c05a87bf4257f82a35
Calus <109810259+Calus426@users.noreply.github.com>
2025-01-07 22:06:42 +0800

    Time: 564 ms (5.04%), Space: 49.9 MB (60.3%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        if(numbers.Length == 2)
+        {
+            return new int[] {1,2};
+        }
+        int first=0;
+        int sec=0;
+        for(int i = 0 ; i<numbers.Length ; i++)
+        {
+            for(int j = i+1 ; j<numbers.Length;j++)
+            {
+                if(numbers[i]+numbers[j]==target)
+                {
+                    first = i+1;
+                    sec=j+1;
+                }
+            }
+        }
+        if(first == 0 && sec == 0)
+        {
+            return new int[] {0,1};
+        }
+        return new int[] {first,sec};
+    }
+}
