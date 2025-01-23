a65d29a8f76bf26d5b198c25bfa6e2759912d367
Calus <109810259+Calus426@users.noreply.github.com>
2025-01-23 22:33:38 +0800

    Time: 13 ms (76.65%), Space: 51.5 MB (23.83%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cs
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cs
@@ -2,7 +2,7 @@ public class Solution {
     public int[] TopKFrequent(int[] nums, int k) {
         Dictionary<int, int> result = new();
 
-        foreach (int num in nums)
+        foreach (int num in nums) //store in dictionary
         {
             if (!result.ContainsKey(num))
             {
@@ -10,7 +10,7 @@ public class Solution {
             }
             result[num]++;
         }
-        var topN = result.OrderByDescending(x => x.Value).Take(k).Select(pair => pair.Key).ToArray();
+        var topN = result.OrderByDescending(x => x.Value).Take(k).Select(pair => pair.Key).ToArray(); //desc for value and get key toArray
 
         return topN;
     }
