fe898a75db1088ecd89fccd1b927b9811fd4c734
Calus <109810259+Calus426@users.noreply.github.com>
2025-11-16 17:16:34 +0800

    Time: 673 ms (97.91%), Space: 0B (100%) - LeetHub

--- 1625-group-sold-products-by-the-date/1625-group-sold-products-by-the-date.sql
+++ 1625-group-sold-products-by-the-date/1625-group-sold-products-by-the-date.sql
@@ -1,7 +1,7 @@
 --Write your MySQL query statement below
 
-SELECT sell_date, count(distinct product ) as num_sold, STRING_AGG(product,',') as products
+SELECT sell_date, count(product) as num_sold, STRING_AGG(product,',') as products
 FROM
-(SELECT DISTINCT * from Activities ) as t
+(SELECT DISTINCT * from Activities ) as t --it will sort the table if use distinct
 GROUP BY sell_date
 
