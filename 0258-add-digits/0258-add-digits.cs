e8a7561d55f726b31ab9c1fefe20e53b7c280fe7
Calus <109810259+Calus426@users.noreply.github.com>
2026-01-28 00:12:18 +0800

    Time: 0 ms (100%), Space: 29 MB (78.09%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int AddDigits(int num) {
+        int count = 0;
+        int f = 0;
+        int s =0;
+
+        if(num <10 && num > 0 ){
+            return num;
+        }
+
+        while(num>=10){
+            f = num / 10;
+            s = num % 10;
+            num  = f+s;
+        }
+        
+        return num;
+    }
+}
